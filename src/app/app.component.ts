af4e124e32bbbd311abc9eeaae758314114d05e5
mozadev <ceosmore@gmail.com>
2024-07-21 19:22:48 -0500

    chore: Add CounterComponent to app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi primera ';
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+
+  resetCounter() {
+    this.counter = 10;
+  }
+
+
+
 }
